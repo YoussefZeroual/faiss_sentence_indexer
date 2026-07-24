feature:token embs are saved in a separate faiss/npy file, fixed many bugs in folder mode, added embedings normalization in file mode

main.py
@@ -52,7 +52,7 @@ def process(args,index, metric_type=faiss.METRIC_INNER_PRODUCT, metadata=None):
         print(f"{r[0]} | {r[1]} |  {r[2]}")
     print("temps d'exécution de la requête Faiss:",np.round(exec_time,2))
 def process_folder(args,input_file):
-        query_vector = embedd_query(args.query)
+        query_vector = embedd_query(args.query,args.token_emb)
         base, ext = os.path.splitext(input_file)
         if '*' in input_file:
             files = glob.glob(input_file)
@@ -61,9 +61,9 @@ def process_folder(args,input_file):
         print("Processing folder: searching similarity in",len(files),"index files")
         if args.force:
             encode_folder(input_file,overwrite=True,token_mode=args.token_emb)
-            makeIndex_folder(input_folder=input_file,metric_type=faiss.METRIC_INNER_PRODUCT,index_type="ivfpq",overwrite=True)
+            makeIndex_folder(input_folder=input_file,metric_type=faiss.METRIC_INNER_PRODUCT,index_type="ivfpq",overwrite=True,token_mode= args.token_emb)
         print("------------")
-        search_folder(input_file,query_vector=query_vector,metric_type=faiss.METRIC_INNER_PRODUCT,top_k=args.top_k)
+        search_folder(input_file,query_vector=query_vector,metric_type=faiss.METRIC_INNER_PRODUCT,top_k=args.top_k,token_mode=args.token_emb)
 def main():
     args = parse_args()
     input_file = args.input_file
@@ -128,28 +128,30 @@ def main():
         query_vector = embedd_query(args.query)
         process_folder(args,base)
         return True
-    if folder or '*' in input_file:
-        if '*' in input_file:
-            process_folder(args,input_file)
-        else:
-            process_folder(args,base)
+    if folder:
+        process_folder(args,base)
         return True
-
+    if'*' in input_file:
+        process_folder(args,input_file)
+        return True
     mode = MODE_BY_EXT.get(ext.lower())
+
     if embeddings_missing and not index_missing and not args.force:
         index = load_index(output_index)
         metadata = load_metadata(output_metadata)
     elif (mode is None) and (not folder) and (embeddings_missing) and (index_missing):
         sys.exit(f"Error: unsupported file extension '{ext}' (expected one of {list(MODE_BY_EXT)})")
 
-    elif embeddings_missing and index_missing:
+    elif (embeddings_missing and index_missing) or args.force:
         embeddings, metadata = calcEmbeddings(input_file, output_embeddings, mode,
                                                reduce_precision=args.reduce_precision,overwrite=args.force,token_mode=args.token_emb)
         print(embeddings.shape)
         save_metadata(metadata, output_metadata)
-        makeIndex(embeddings=embeddings, embedding_file_path=None,
+        index=makeIndex(embeddings=embeddings, embedding_file_path=None,
                                metric_type=faiss.METRIC_INNER_PRODUCT,
                                index_type=args.index_type, output_file_path=output_index)
+        embeddings_missing = False
+        index_missing = False
 
     else:
         embeddings = np.load(output_embeddings)

makeIndex.py
@@ -13,12 +13,12 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 def load_embeddings(embedding_file_path):
-    embeddings = np.load(embedding_file_path)
-
+    base,ext = os.path.splitext(embedding_file_path)
+    embeddings = np.load(base+".npy")
     embeddings = np.ascontiguousarray(embeddings,dtype=np.float32)
     faiss.normalize_L2(embeddings)
     return embeddings
-def makeIndex(embeddings=None,embedding_file_path=None,metric_type=None,index_type=None,output_file_path=None,overwrite=False):
+def makeIndex(embeddings=None,embedding_file_path=None,metric_type=None,index_type=None,output_file_path=None,overwrite=False,token_mode=False):
     import os
     if embedding_file_path is not None:
         base, ext = os.path.splitext(embedding_file_path)
@@ -29,6 +29,9 @@ def makeIndex(embeddings=None,embedding_file_path=None,metric_type=None,index_ty
             return index
     if embeddings is None:
        embeddings = load_embeddings(embedding_file_path)
+    else:
+        embeddings = np.ascontiguousarray(embeddings,dtype=np.float32)
+        faiss.normalize_L2(embeddings)
     logger.info("making index for %s sentences, index type=%s",embeddings.shape[0],index_type)
     n,dim = embeddings.shape
     metric_type = metric_type
@@ -56,28 +59,34 @@ def makeIndex(embeddings=None,embedding_file_path=None,metric_type=None,index_ty
     else:
         logger.warning("Index Type unrecognized or empty: %s",index_type)
         raise ValueError(f"Index type unkown or empty: {index_type}")
-
+    if token_mode:
+        output_file_path = output_file_path.replace(".faiss","_token.faiss")
     faiss.write_index(index,output_file_path)
     t1 = time.perf_counter()
     exec_time = t1-t0
     logger.info("Index created successfully in %s seconds",np.round(exec_time,3))
-    logger.info("index written successfully")
+    logger.info("index written successfully to %s",output_file_path)
 
 
     return index
-def makeIndex_folder(input_folder=None,metric_type=None,index_type=None,overwrite=False):
+def makeIndex_folder(input_folder=None,metric_type=None,index_type=None,overwrite=False,token_mode=False):
+    if token_mode:
+        emb_ext = "_token.npy"
+    else:
+        emb_ext = ".npy"
     if '*' in input_folder:
         file_list = glob.glob(input_folder)
-        file_list = [os.path.splitext(f)[0]+".npy"  for f in file_list]
+        file_list = [os.path.splitext(f)[0].replace("_token","")+emb_ext  for f in file_list]
     else:
-        file_list = glob.glob(input_folder+"/*"+"npy")
+        file_list = glob.glob(input_folder+"/*"+emb_ext)
     len_f = len(file_list)
     logger.info("Found %s files in folder",len_f)
     for f in file_list:
         logger.info("%s",f)
     for f in file_list:
+        base,ext = os.path.splitext(f)
         logger.info("Making index for file %s",f)
-        output_file_path = f.replace(".npy",".faiss")
+        output_file_path = base+".faiss"
         index = makeIndex(embedding_file_path=f,metric_type=metric_type,index_type=index_type,output_file_path=output_file_path,overwrite=overwrite)
 
 

searchEmbedding.py
@@ -33,6 +33,10 @@ def load_metadata(matadata_file_path=None):
         raise ValueError("Couldnt load metadata")
 
 def embedd_query(query_str=None,token_mode=False):
+    if token_mode:
+        logger.info("Encoding query with token level mode")
+    else:
+              logger.info("Encoding query with sentence level mode")
     import time
     t0 = time.perf_counter()
     if query_str is not None:
@@ -63,22 +67,39 @@ def search(query_vector=None,query_str=None, index=None, metric_type=None, top_k
     faiss.normalize_L2(query_vector)
     if hasattr(index, "nprobe"):
         index.nprobe = 8
-    distances, indices = index.search(query_vector, top_k)
-    matches = [(metadata["sent_id"][idx],metadata["raw_text"][idx], np.round(float(distance),3))
-               for idx, distance in zip(indices[0], distances[0])
-               if 0 <= idx < len_metadata]
+    try:
+        distances, indices = index.search(query_vector, top_k)
+        matches = [(metadata["sent_id"][idx],metadata["raw_text"][idx], np.round(float(distance),3))
+                for idx, distance in zip(indices[0], distances[0])
+                if 0 <= idx < len_metadata]
+    except AssertionError as e:
+        logger.warning("Assert error: query was probably encoded using a different model than target embeddings, please reembed target texts")
+        return []
     return matches
-def search_folder(input_folder=None,query_str=None,query_vector=None,metric_type=faiss.METRIC_INNER_PRODUCT,top_k=10,verbose=True):
+def search_folder(input_folder=None,query_str=None,query_vector=None,metric_type=faiss.METRIC_INNER_PRODUCT,top_k=10,verbose=True,token_mode=False):
     import time
     t0 = time.perf_counter()
     logger.info("Folder embedding search")
     import glob
     import os
+    token_suffix = ""
+
+    if token_mode:
+        index_ext = "_token.faiss"
+        token_suffix = "_token"
+    else:
+       index_ext = ".faiss"
     if '*' in input_folder:
         file_list = glob.glob(input_folder)
-        file_list = [os.path.splitext(f)[0]+".faiss"  for f in file_list]
+        file_list = [os.path.splitext(f)[0].replace("_token","")+index_ext  for f in file_list]
     else:
-        file_list = glob.glob(input_folder+"/*"+"faiss")
+        file_list = glob.glob(input_folder+"/*"+index_ext)
+
+    if token_mode:
+        file_list = list(set([f for f in file_list if os.path.splitext(f)[1] ==".faiss" and '_token' in f]))
+    else:
+        file_list = list(set([f for f in file_list if os.path.splitext(f)[1] ==".faiss"]))
+    print(file_list)
     len_f = len(file_list)
     results = []
     skipped = False
@@ -86,19 +107,24 @@ def search_folder(input_folder=None,query_str=None,query_vector=None,metric_type
     if query_vector is not None:
         query_vector =query_vector
     else:
-        query_vector = embedd_query(query_str)
+        query_vector = embedd_query(query_str,token_mode)
     faiss.normalize_L2(query_vector)
     for f in file_list:
         base, ext = os.path.splitext(f)
-        logger.info("%s",f)
-        index = load_index(f)
         try:
-            metadata = load_metadata(base+".json")
+            index = load_index(f)
+        except RuntimeError as e:
+            logger.warning("%s index file not found in directory,skipping file",base+".faiss")
+            skipped = True
+            continue
+        try:
+            metadata = load_metadata(base.replace("_token","")+".json")
         except FileNotFoundError as e:
             logger.warning("%s metadata file not found in directory,skipping file",base+".json")
             skipped = True
             continue
-        result = search(query_str=query_str,query_vector=query_vector, index=index, metric_type=metric_type, top_k=top_k, metadata=metadata)
+
+        result = search(query_str=query_str,query_vector=query_vector, index=index, metric_type=metric_type, top_k=top_k, metadata=metadata,token_mode=token_mode)
         result = [(f,r[0],r[1],float(r[2]))
                for r  in result]
         results.extend(result)
@@ -108,7 +134,9 @@ def search_folder(input_folder=None,query_str=None,query_vector=None,metric_type
     exec_time = t1-t0
     logger.info("Folder search executed in %s seconds in %s files",np.round(exec_time,2),len_f)
     if skipped:
-        logger.warning("Some index files were skipped because corresponding metadata files were not found")
+        logger.warning("Some index files were skipped because file or corresponding metadata files were not found")
+    if results ==[]:
+        logger.warning("Search query didn't return any results, input file list probaby empty")
     if verbose:
         print("index file                | Sent id               | Sentence    | similarity score")
         for r in results: